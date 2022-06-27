04b384bc9f773fede6393963d91aa101cc40bedf
Raffy L. Doquenia <radoq10288@gmail.com>
2022-06-27 22:49:23 +0800

    Add and implement help() and version() function
    To show user how to use the software properly and its current version.

--- src/main.c
+++ src/main.c
@@ -6,6 +6,28 @@
 #include <string.h>
 
 
+static void help(void) {
+	printf("\nUsage:\n"
+			"    cpps [Project Name] [Option]     For creating a C programming project.\n"
+			"    cpps [Option]                    For other options to show help, version, etc.\n"
+			"\n"
+			"Options:\n"
+			"    -e, --exe-name     Filename of the executable file to be created.\n"
+			"    -f, --filename     Filename of the C source file to be created.\n"
+			"    -h                 Show this help.\n"
+			"        --verbose      Show messages what is being done.\n"
+			"    -v, --version      Show current version of this software.\n");
+}
+
+
+static void version(void) {
+	printf("\nC Programming Project Setup version 0.1.1-pre-alpha.6, Copyright (C) 2022 Radoq10288\n"
+			"C Programming Project Setup comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\n"
+			"This is free software, and you are welcome to redistribute it\n"
+			"under certain conditions; type `show c' for details.\n");
+}
+
+
 int main(int argc, char *argv[]) {
 	char c_source_filename[260] = "main", c_source_file_loc[260] = "projectname\\src\\",
 		 exe_filename[260], project_name[260];
@@ -19,29 +41,34 @@ int main(int argc, char *argv[]) {
 	while(true) {
         int option_index = 0;
         static struct option long_options[] = {
-			{"brief",		no_argument,       	&flag,				0},
-            {"exe-name",	required_argument,	0,  				'e'},
-            {"filename",	required_argument,	0,  				'f'},
-            {"verbose",		no_argument,		&flag,			1},
-            {0,				0,					0,					0}
+            {"exe-name",	required_argument,	0,  		'e'},
+            {"filename",	required_argument,	0,  		'f'},
+            {"help",		no_argument,		0,  		'h'},
+            {"version",		no_argument,		0,			'v'},
+			{"brief",		no_argument,       	&flag,		0},
+			{"verbose",		no_argument,		&flag,		1},
+            {0,				0,					0,			0}
         };
 
 		opterr = 0;
-		getopt_status = getopt_long(argc, argv, ":e:f:", long_options, &option_index);
+		getopt_status = getopt_long(argc, argv, ":e:f:hv", long_options, &option_index);
         if (getopt_status == -1) {
 			break;
 		}
 
 		switch(getopt_status) {
-			case 0:
-				// Just do nothing here...
-				break;
 			case 'e':
 				strcpy(exe_filename, optarg);
 				break;
 			case 'f':
 				strcpy(c_source_filename, optarg);
 				break;
+			case 'h':
+				help();
+				goto skip_project_creation;
+			case 'v':
+				version();
+				goto skip_project_creation;
 			case '?':
 				if (optopt == '\0') {
 					fprintf(stderr, "cpps\nError: Unknown long option of '%s'\n", argv[optind - 1]);
@@ -71,6 +98,7 @@ int main(int argc, char *argv[]) {
 
 	if (flag == 1)  {printf("New project \"%s\" is created.\n", project_name);}
 
+	skip_project_creation:;
 	return 0;
 
 	cpps_error:;
