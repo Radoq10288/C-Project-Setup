a00bc56567641937958cd5ed3af35f0527fae02c
Raffy L. Doquenia <radoq10288@gmail.com>
2023-01-31 22:13:30 +0800

    Refactor make_project() function
    Due to the removal of the two functions make_csf(), and make_makefile(),
    make_project() is refactored for the new smaller code base.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,88 @@
 #include <getopt.h>
-#include "functions.h"
+#include <ctype.h>
+
+/* This header file will only be available when compiling
+ * CPS under Windows using MinGW.
+ */
+#ifndef __linux__
+	#include <io.h>
+#endif
+
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>	// mkdir() function resides here in linux
+#include <time.h>
+
+
+#define EMPTY_STRING	"\0"
+
+
+static char c_file[500] = {
+	"/* File:         %s\n"
+	" *\n"
+	" * Author:       \n"
+	" *\n"
+	" * Date & time:  %s-%s\n"
+	" */\n"
+	"\n"
+	"#include <stdio.h>\n"
+	"\n"
+	"\n"
+	"int main() {\n"
+	"    // Your code goes here...\n"
+	"    return 0;\n"
+	"}\n"
+	"\n"
+	"\n"
+};
+
+
+static char makefile[650] = {
+	"BINDIR=bin\n"
+	"OBJDIR=obj\n"
+	"SRCDIR=src\n"
+	"\n"
+	"OS=$(shell uname -o)\n"
+	"ifeq ($(OS), Msys)\n"
+	"BINFILE=%s.exe\n"
+	"endif\n"
+	"ifeq ($(OS), GNU/Linux)\n"
+	"BINFILE=%s\n"
+	"endif\n"
+	"\n"
+	"BIN=$(BINDIR)/$(BINFILE)\n"
+	"OBJ=$(OBJDIR)/*.o\n"
+	"CFILES=$(SRCDIR)/*.c\n"
+	"\n"
+	"CC=gcc\n"
+	"\n"
+	"debug: CFLAGS=-g -pedantic -Wall\n"
+	"debug: build compile\n"
+	"\n"
+	"release: CFLAGS=-O2 -Wall\n"
+	"release: build compile\n"
+	"\n"
+	"compile: $(BIN)\n"
+	"$(BIN) : $(OBJ)\n"
+	"\t$(CC) -o $(BIN) $(OBJ)\n"
+	"\n"
+	"build: $(OBJ)\n"
+	"$(OBJ) : $(CFILES)\n"
+	"\t$(CC) -c $(CFILES) $(CFLAGS)\n"
+	"\n"
+	"\t@mkdir -p $(BINDIR)\n"
+	"\t@mkdir -p $(OBJDIR)\n"
+	"\t@mv *.o $(OBJDIR)/\n"
+	"\n"
+	"clean:\n"
+	"\trm $(OBJ) $(BINDIR)/*\n"
+	"\n"
+	"distclean: clean\n"
+	"\trmdir $(BINDIR) $(OBJDIR)\n"
+	"\n\n"
+};
 
 
 static void help(void) {
@@ -26,6 +106,98 @@ static void version(void) {
 			"under certain conditions; type `show c' for details.\n");
 }
 
+
+static void get_date(char *date_str) {
+	char date_string[11];
+	time_t date;
+	struct tm *date_info;
+
+	time(&date);
+	date_info = localtime(&date);
+	strftime(date_string, 11, "%m/%d/%Y", date_info);
+	strcpy(date_str, date_string);
+}
+
+
+static void get_time(char *time_str) {
+	char time_string[12];
+	time_t time_;
+	struct tm *time_info;
+
+	time(&time_);
+	time_info = localtime(&time_);
+	strftime(time_string, 12, "%I:%M:%S-%p", time_info);
+	strcpy(time_str, time_string);
+}
+
+
+static int make_file(const char *file_name, const char *file_content) {
+	FILE *new_file;
+	if ((new_file = fopen(file_name, "r")) == NULL) {
+		if ((new_file = fopen(file_name, "w")) == NULL) {
+			goto makefile_error;
+		}
+		fputs(file_content, new_file);
+		fclose(new_file);
+	}
+	else {
+		fclose(new_file);
+		goto makefile_error;
+	}
+
+	return 0;
+
+	makefile_error:;
+	return 1;
+}
+
+
+static int make_dir(const char *dir_name) {
+	#ifdef __linux__
+		if (mkdir(dir_name, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH)) {goto makedir_error;}
+	#else
+		if (_mkdir(dir_name)) {goto makedir_error;}
+	#endif
+	return 0;
+
+	makedir_error:;
+	return 1;
+}
+
+
+static char content_string[600] = EMPTY_STRING;
+static char date_string[11] = EMPTY_STRING;
+static char time_string[12] = EMPTY_STRING;
+
+
+static int make_project(char *project_name, char *exe_name, char *c_source_file) {
+	char bin_dir_location[260], src_dir_location[260], src_file[260], makefile_name[260];
+
+	if (make_dir(project_name)) {return 1;}
+
+	sprintf(bin_dir_location, "%s/bin", project_name);
+	sprintf(src_dir_location, "%s/src", project_name);
+	make_dir(bin_dir_location);
+	make_dir(src_dir_location);
+	
+	get_date(date_string);
+	get_time(time_string);
+	strcat(src_file, ".c");
+	
+	sprintf(src_file, "%s/src/%s", project_name, c_source_file);
+	sprintf(content_string, c_file, src_file, date_string, time_string);
+	make_file(src_file, content_string);
+	
+	sprintf(makefile_name, "%s/Makefile", project_name);
+	sprintf(content_string, makefile, exe_name);
+	make_file(makefile_name, content_string);
+
+	return 0;
+}
+
+// This is only used for testing purposes.
+//#define ON_TEST_MODE
+#ifndef ON_TEST_MODE
 
 int main(int argc, char *argv[]) {
 	char c_source_filename[260] = "main", exe_filename[260], project_name[260];
@@ -97,5 +269,7 @@ int main(int argc, char *argv[]) {
 	cpps_error:;
 	return 1;
 }
+
+#endif
 
 
